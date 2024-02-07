0b1498aa7094a87d5bd64f8b23d0e126f4b57848
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-02-07 17:58:09 +0000

    subtract form and results work

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "addition", :action => "show_addition_form" })
-
+#add
   get("/add", { :controller => "addition", :action => "show_addition_form" })
+
   get("/wizard_add", { :controller => "addition", :action => "add_these" })
 
-  get("/subtract", { :controller => "subtract", :action => "show_sub_form" })
-  get("", { :controller => "subtraction", :action => "subtract" })
+  #subtract
+  get("/subtract", { :controller => "subtraction", :action => "show_subtraction_form" })
 
+  get("/wizard_subtract", { :controller => "subtraction", :action => "subtract_these" })
+
+#multiply
   get("/multiply", { :controller => "multiplication", :action => "multiplication" })
-  get("/wizard_multiply", { :controller => "multiplication", :action => "multiply" })
 
+  get("/wizard_multiply", { :controller => "multiplication", :action => "multiply" })
+#divide
   get("/divide", { :controller => "division", :action => "show_division_form" })
   get("/wizard_div", { :controller => "div", :action => "divide_these" })
 
